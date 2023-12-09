4408a68a42d57b29b9ddb38be996fe3a9c3bba7c
albertbokor-unideb <128703802+albertbokor-unideb@users.noreply.github.com>
2023-12-09 19:45:23 +0100

    initial commit

---
+++ src/main/java/com/example/jws9ixbead/Jws9ixBeadApplication.java
@@ -0,0 +1,13 @@
+package com.example.jws9ixbead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jws9ixBeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jws9ixBeadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jws9ixbead/Jws9ixBeadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jws9ixbead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jws9ixBeadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
