dd167ddc13d5e08f5eafac7947d789f4d4ff9d4a
albertbokor-unideb <128703802+albertbokor-unideb@users.noreply.github.com>
2023-12-09 21:54:31 +0100

    feature: implement methods for BookController

--- src/main/java/com/example/jws9ixbead/controller/BookController.java
+++ src/main/java/com/example/jws9ixbead/controller/BookController.java
@@ -2,42 +2,64 @@ package com.example.jws9ixbead.controller;
 
 import com.example.jws9ixbead.model.Book;
 import com.example.jws9ixbead.repository.BookRepo;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
     @Autowired
     private BookRepo bookRepo;
-    @GetMapping
+    @GetMapping("/getBookAll")
     public ResponseEntity<List<Book>> getBookAll(){
-        
+        try{
+            List<Book> bookList = new ArrayList<>();
+            bookRepo.findAll().forEach(bookList::add);
+            if(bookList.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch (Exception ex){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
-    @GetMapping
-    public void getBookByISBN(){
-
+    @GetMapping("/getBookByISBN/{isbn}")
+    public ResponseEntity<Book> getBookByISBN(@PathVariable Long isbn){
+        Optional<Book> bookData =bookRepo.findById(isbn);
+        if(bookData.isPresent()){
+            return new ResponseEntity<>(bookData.get(),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-    @PostMapping
-    public void addBook(){
-
+    @PostMapping("/addBook")
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
+        Book bookObject = bookRepo.save(book);
+        return new ResponseEntity<>(bookObject,HttpStatus.OK);
     }
-    @PostMapping
-    public void updateBookByISBN(){
 
+    @PostMapping("/updateBookByISBN")
+    public ResponseEntity<Book> updateBookByISBN(@PathVariable Long isbn, @RequestBody Book newData){
+        Optional<Book> oldData =bookRepo.findById(isbn);
+        if(oldData.isPresent()){
+            Book updatedData = oldData.get();
+            updatedData.setTitle(newData.getTitle());
+            updatedData.setAuthor(newData.getAuthor());
+            Book bookObj = bookRepo.save(updatedData);
+            return new ResponseEntity<>(bookObj,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     @DeleteMapping
-    public void deleteBookByISBN(){
-
+    public ResponseEntity<HttpStatus> deleteBookByISBN(@PathVariable Long isbn){
+        bookRepo.deleteById(isbn);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
